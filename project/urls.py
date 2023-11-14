a3ec9714c7d5168d16d55275a8ea7a5d923b41dd
Islam412 <islam.124.hamdy@gmail.com>
2023-11-14 03:41:22 +0200

    add views say_hello p1

--- project/urls.py
+++ project/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/',include('playground.urls'))
+    path('playground/',include('playground.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
