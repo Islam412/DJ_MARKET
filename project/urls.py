ad3b38a86cf31dc4eb96e4baba80222e0ca814d0
Islam412 <islam.124.hamdy@gmail.com>
2023-11-15 04:03:02 +0200

    desing product & customer models p1

--- project/urls.py
+++ project/urls.py
@@ -18,14 +18,12 @@ from django.contrib import admin
 from django.urls import path ,include
 from django.conf import settings
 from django.conf.urls.static import static
-# import debug_toolbar
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/',include('playground.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
-
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=10000)
+    price = models.DecimalField(max_digits=6 , decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+
+
+class Customer(models.Model):
+    frist_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
